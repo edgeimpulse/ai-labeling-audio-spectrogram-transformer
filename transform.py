b7592d7fcdbc61b8c73fd85e520957368254298f
Ivan Turasov <turasov.ivan@gmail.com>
2024-07-26 19:00:02 +0200

    Add preferred labels, fix parameters

--- transform.py
+++ transform.py
@@ -1,6 +1,7 @@
-import os
+import os, sys
 import requests
 import time
+import argparse
 
 # Import the API objects we plan to use
 from edgeimpulse_api import ApiClient, Configuration, ProjectsApi, RawDataApi
@@ -10,26 +11,41 @@ from edgeimpulse_api.models.set_sample_structured_labels_request import (
 
 ### This is a standalone block
 
+# Multi-label is only available for pro and enterprise;
+# Ingest multi-label: https://docs.edgeimpulse.com/docs/tutorials/api-examples/ingest-multi-label-data-api
+# Just add the label to the existing sample:
+####
+# 1. Get the audio file from the project (from the API)
+# 2. create_splits_and_classify
+#   2.1 If Multi-label is enabled, split, create structured labels and push back the labels to the sample
+#   2.2 If Multi-label is disabled, split and upload labeled files to the project
+
+# Multi-label is only available for pro and enterprise;
+# Ingest multi-label: https://docs.edgeimpulse.com/docs/tutorials/api-examples/ingest-multi-label-data-api
+# Just add the label to the existing sample: https://github.com/edgeimpulse/example-multi-label-ingestion-via-api/blob/master/update-sample.sh
+
+# If community upload back splits with labels
 
 # For splitting audio
 from pydub import AudioSegment
 
-# # Set and retrieve env. variables
-# if not os.getenv("HF_API_KEY"):
-#     print("Missing HF_API_KEY")
-#     sys.exit(1)
+# Set and retrieve env. variables
+if not os.getenv("HF_API_KEY"):
+    print("Missing HF_API_KEY")
+    sys.exit(1)
 
-### Property of the project
-# if not os.getenv("EI_PROJECT_API_KEY"):
-#     print("Missing EI_PROJECT_API_KEY")
-#     sys.exit(1)
+## Property of the project
+if not os.getenv("EI_PROJECT_API_KEY"):
+    print("Missing EI_PROJECT_API_KEY")
+    sys.exit(1)
 
 HF_API_KEY = os.environ.get("HF_API_KEY")
 EI_PROJECT_API_KEY = os.environ.get("EI_PROJECT_API_KEY")
+
 EI_INGESTION_HOST = os.environ.get("EI_INGESTION_HOST", "edgeimpulse.com")
 
-HF_API_KEY = ""
-EI_PROJECT_API_KEY = ""
+# HF_API_KEY = ""
+# EI_PROJECT_API_KEY = ""
 
 # Inferencing of audio classifier
 HF_INFERENCE_URL = "https://api-inference.huggingface.co/models/MIT/ast-finetuned-audioset-10-10-0.4593"
@@ -39,77 +55,62 @@ EI_API_HOST = "https://studio.edgeimpulse.com/v1"
 DATASET_PATH = "dataset/gestures"
 OUTPUT_PATH = "./out"
 
-# # Argument parser for command line arguments
+# Argument parser for command line arguments
 
-# parser = argparse.ArgumentParser(
-#     description="Use Hugging Face to classify sound sample in your dataset"
-# )
+parser = argparse.ArgumentParser(
+    description="Use Hugging Face to classify sound sample in your dataset"
+)
 # parser.add_argument(
 #     "--in-file",
 #     type=str,
 #     required=False,
 #     help="Argument passed by Edge Impulse transformation block when the --in-file option is selected",
 # )
-# parser.add_argument(
-#     "--out-directory",
-#     type=str,
-#     required=False,
-#     help="Directory to save images to",
-#     default="output",
-# )
-# parser.add_argument(
-#     "--audioset-labels",
-#     type=str,
-#     required=True,
-#     help='Comma separated list of labels from "AudioSet" that will be be used to label the sample. When model returns any other label, the label for this sample in Edge Impulse will be set to "noise"',
-# )
+parser.add_argument(
+    "--out-directory",
+    type=str,
+    required=False,
+    help="Directory to save images to",
+    default="output",
+)
+parser.add_argument(
+    "--audioset-labels",
+    type=str,
+    required=False,
+    help='Comma separated list of labels from "AudioSet" that will be be used to label the sample. When model returns any other label, the label for this sample in Edge Impulse will be set to "noise". If no labels set here all the results will be added do the model',
+)
+parser.add_argument(
+    "--win-size-ms",
+    type=int,
+    required=True,
+    help="Size of the window for each classification",
+)
+parser.add_argument(
+    "--win-stride-ms",
+    type=int,
+    required=True,
+    help="Size of the window for each classification",
+)
+parser.add_argument(
+    "--maybe-enabled",
+    type=bool,
+    required=False,
+    help='If two or more highest classes are not distinctively different, the model will return "maybe" as a label',
+)
 
-# parser.add_argument(
-#     "--win-size-ms",
-#     type=int,
-#     required=True,
-#     help="Size of the window for each classification",
-# )
-# parser.add_argument(
-#     "--upload-category",
-#     type=str,
-#     required=False,
-#     help="Which category to upload data to in Edge Impulse",
-#     default="split",
-# )
-# parser.add_argument(
-#     "--synthetic-data-job-id",
-#     type=int,
-#     required=False,
-#     help="If specified, sets the synthetic_data_job_id metadata key",
-# )
-# parser.add_argument(
-#     "--skip-upload",
-#     type=bool,
-#     required=False,
-#     help="Skip uploading to EI",
-#     default=False,
-# )
+args, unknown = parser.parse_known_args()
 
-# args, unknown = parser.parse_known_args()
+audioset_labels_list = args.audioset_labels.split(",")
+print(f"Labels to get out of the model: {audioset_labels_list}")
 
-# # Set up ingestion URL
-# INGESTION_URL = f"https://ingestion.{INGESTION_HOST}"
-# if INGESTION_HOST.endswith(".test.edgeimpulse.com"):
-#     INGESTION_URL = f"http://ingestion.{INGESTION_HOST}"
-# if INGESTION_HOST == "host.docker.internal":
-#     INGESTION_URL = f"http://{INGESTION_HOST}:4810"
+win_size_ms = args.win_size_ms
+print(f"Window size: {win_size_ms}")
 
-# if os.path.exists(args.out_directory):
-#     shutil.rmtree(args.out_directory)
-# os.makedirs(args.out_directory)
+win_stride_ms = args.win_stride_ms
+print(f"Window stride: {win_stride_ms}")
 
-# current epoch time
-epoch = int(time.time())
-
-# print("Prompt:", args.prompt)
-# print("Number of images:", args.images)
-# print("")
+is_maybe_enabled = args.maybe_enabled
+print(f"Maybe enabled: {is_maybe_enabled}")
 
 
 def classify_audio_sample(filename: str, hf_api_key: str):
@@ -155,11 +156,12 @@ def create_splits_and_classify_from_wav(
     win_size_ms: int,
     stride_ms: int,
     hf_api_key: str,
+    audioset_labels_list: list = None,
 ):
     """
     Split the input audio file into windows of size "win_size_ms" with stride "stride_ms"
     Then put each split though audio classifier and rename the file with the label
-    If the multilabel_output is enabled, return a structured multilable list and files list
+    If the multilabel_output is enabled, return a structured multilabel list and files list
     """
 
     # Load the input audio file
@@ -201,6 +203,10 @@ def create_splits_and_classify_from_wav(
         classification = classify_audio_sample(output_file, hf_api_key)
         print(classification)
         label = classification[0]["label"]
+
+        if audioset_labels_list:
+            if label not in audioset_labels_list:
+                label = "noise"
 
         # Rename, appending label as first token
         fname_split_labeled = f"{label}.{fname}_{i}_{i + win_size_ms}.wav"
@@ -291,12 +297,6 @@ def get_sample_wav_from_project_by_id(project_id, sample_id, path):
 ### Begin entry point
 #####################
 
-# # Set output directory
-# if not os.path.exists(args.out_directory):
-#     os.makedirs(args.out_directory)
-# output_folder = args.out_directory
-
-
 # Create top-level API client
 config = Configuration(
     host=EI_API_HOST, api_key={"ApiKeyAuthentication": EI_PROJECT_API_KEY}
@@ -337,6 +337,8 @@ response = raw_data_api.list_samples(project_id=project_id, category="testing")
 samples.append(response.to_dict()["samples"])
 
 for sample in samples:
+    if "id" not in sample:
+        continue
     sample_id = sample["id"]
     sample_filename = sample["filename"]
 
@@ -356,7 +358,12 @@ for sample in samples:
 
     # Create splits and classify
     intervals_list, files_list = create_splits_and_classify_from_wav(
-        sample_file_path, OUTPUT_PATH, 5000, 2000, HF_API_KEY
+        sample_file_path,
+        OUTPUT_PATH,
+        win_size_ms,
+        win_stride_ms,
+        HF_API_KEY,
+        audioset_labels_list,
     )
 
     # Create structuredLabels.labels and append to sample
@@ -366,18 +373,3 @@ for sample in samples:
     append_multilabel_to_sample_in_studio(
         raw_data_api, project_id, sample_id, structured_labels
     )
-
-# Multi-label is only available for pro and enterprise;
-# Ingest multi-label: https://docs.edgeimpulse.com/docs/tutorials/api-examples/ingest-multi-label-data-api
-# Just add the label to the existing sample:
-####
-# 1. Get the audio file from the project (from the API)
-# 2. create_splits_and_classify
-#   2.1 If Multi-label is enabled, split, create structured labels and push back the labels to the sample
-#   2.2 If Multi-label is disabled, split and upload labeled files to the project
-
-# Multi-label is only available for pro and enterprise;
-# Ingest multi-label: https://docs.edgeimpulse.com/docs/tutorials/api-examples/ingest-multi-label-data-api
-# Just add the label to the existing sample: https://github.com/edgeimpulse/example-multi-label-ingestion-via-api/blob/master/update-sample.sh
-
-# If community upload back splits with labels
